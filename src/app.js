549f964508bb818b38c0355a3782d57f06838564
majorabrito <marialeirabrito@gmail.com>
2026-02-20 14:32:10 +0100

    Installed API and AI, deleted autofocus from language

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
+function displayDescription(response){
+
+let descriptionElement = document.querySelector("#descrption-element");
+  descriptionElement.innerHTML = "";
+
+    new Typewriter("#descrption-element", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 0.5,
+    cursor: "",
+  });
+}
+
+
+
 function descriptionGenerator(event){
     event.preventDefault();
 
-  new Typewriter("#descrption-element", {
-    strings: "here is the word descrption",
-    autoStart: true,
-    delay: 0.5,
-    cursor: "",5
-  });
+let userWord = document.querySelector("#word").value;
+let userLanguage= document.querySelector("#language").value;
+
+  let apiKey = "684391fb9be129et40f040f4bb35oaff";
+
+let context =`You are a professional polyglot. Your goal is to translate the word into ${userLanguage} and then write a brief description and usage example ENTIRELY in ${userLanguage}. The format has to be
+ <strong>Translated Word</strong> then <br /> you must display a brief description in ${userLanguage}. Max 200 characthers`;
+
+let prompt = `Word: ${userWord}. Target Language: ${userLanguage}. Provide the translation and its description in ${userLanguage}.`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayDescription);
 }
 
 let formElement = document.querySelector("#form-element");
