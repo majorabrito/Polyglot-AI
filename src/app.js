8fbdaa620950cae0ff004211d820fc87b4cd4c5c
majorabrito <marialeirabrito@gmail.com>
2026-02-20 15:10:31 +0100

    Added more context

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ function descriptionGenerator(event) {
   let userLanguage = document.querySelector("#language").value;
   let apiKey = "684391fb9be129et40f040f4bb35oaff";
 
-  let context = `You are a professional polyglot. Your goal is to translate the word into ${userLanguage} and then write a brief description in ${userLanguage}. The format has to be <strong>Translated Word</strong> then <br /> you must display a brief description in ${userLanguage}. Max 200 characters.`;
+  let context = `You are a professional polyglot. Your goal is to translate the word into ${userLanguage} and then write a brief description in ${userLanguage}. The format has to be <strong>Translated Word</strong> then <br /> you must display a brief description in ${userLanguage}. Max 200 characters. Include the origin of the word and the country that is most used`;
   
   let prompt = `Word: ${userWord}. Target Language: ${userLanguage}. Provide the translation and its description in ${userLanguage}.`;
 
